b0f7cfb4824aae8773b1966ef6d527df372d329d
Sam Verschueren <sam.verschueren@gmail.com>
2015-11-26 08:21:11 +0100

    Resolve #6 - Add XO

--- index.js
+++ index.js
@@ -3,61 +3,48 @@
 /**
  * Sets the access origins of the cordova project.
  *
- * @author Sam Verschueren      <sam.verschueren@gmail.com>
+ * @author Sam Verschueren	  <sam.verschueren@gmail.com>
  * @since  21 May 2015
  */
 
 // module dependencies
-var path = require('path'),
-    through = require('through2'),
-    gutil = require('gulp-util'),
-    Config = require('cordova-config');
+var path = require('path');
+var through = require('through2');
+var gutil = require('gulp-util');
+var Config = require('cordova-config');
 
 // export the module
-module.exports = function(origin, options) {
+module.exports = function (origin, options) {
+	var project;
+	var origins = origin;
 
-    var project,
-        origins = origin;
+	if (typeof origin !== 'object') {
+		// If origin is not an object, it is the origin value
+		origins = {};
+		origins[origin] = options;
+	}
 
-    if(typeof origin !== 'object') {
-        // If origin is not an object, it is the origin value
-        origins = {};
-        origins[origin] = options;
-    }
+	return through.obj(function (file, enc, cb) {
+		project = file;
 
-    return through.obj(function(file, enc, cb) {
-        project = file;
+		this.push(file);
 
-        // Pipe the file to the next step
-        this.push(file);
+		try {
+			var config = new Config(path.join(project.path, 'config.xml'));
 
-        cb();
-    }, function(cb) {
-        try {
-            // Load the config.xml file
-            var config = new Config(path.join(project.path, 'config.xml'));
+			Object.keys(origins).forEach(function (origin) {
+				if (origins[origin] === false) {
+					config.removeAccessOrigin(origin);
+				} else {
+					config.setAccessOrigin(origin, origins[origin] === true ? {} : origins[origin]);
+				}
+			});
 
-            // Iterate over the access origins and add them to the config file
-            for(var key in origins) {
-                if(origins[key] === false) {
-                    // If the value of the origin is false, remove it from the config file
-                    config.removeAccessOrigin(key);
-                }
-                else {
-                    // Set the access origin                  
-                    config.setAccessOrigin(key, origins[key] === true ? {} : origins[key]);
-                }
-            }
-
-            // Write the config file
-            config.write(function() {
-				// Call the callback
+			config.write(function () {
 				cb();
 			});
-        }
-        catch(err) {
-			// Oh no, something happened!
-            cb(new gutil.PluginError('gulp-cordova-access', err.message));
-        }
-    });
+		} catch (err) {
+			cb(new gutil.PluginError('gulp-cordova-access', err.message));
+		}
+	});
 };
--- test/test.js
+++ test/test.js
@@ -3,345 +3,342 @@
 /**
  * Test runner for gulp-cordova-access
  *
- * @author Sam Verschueren      <sam.verschueren@gmail.com>
+ * @author Sam Verschueren	  <sam.verschueren@gmail.com>
  * @since  6 Sep. 2015
  */
 
 // module dependencies
-var chai = require('chai'),
-    tempfile = require('tempfile'),
-    fs = require('fs-extra'),
-    path = require('path'),
-    os = require('os'),
-    gutil = require('gulp-util');
+var chai = require('chai');
+var tempfile = require('tempfile');
+var fs = require('fs-extra');
+var path = require('path');
+var os = require('os');
+var gutil = require('gulp-util');
 
 // use should flavour
 var should = chai.should();
 
 var access = require('../');
 
-describe('gulp-cordova-access', function() {
-
-    beforeEach(function() {
-        this.tmp = tempfile();
-
-        fs.copySync(path.join(__dirname, '/fixtures/config.xml'), path.join(this.tmp, 'config.xml'));
-    });
-
-    it('Should throw an error if something went wrong', function(cb) {
-        var tmp = tempfile();
-
-        fs.copySync(path.join(__dirname, '/fixtures/config.empty.xml'), path.join(tmp, 'config.xml'));
-
-        // Create the stream
-        var stream = access('*', false);
-
-        stream.on('error', function(err) {
-            cb();
-        });
-
-        // Write the file to the stream
-        stream.write(new gutil.File({
-            cwd: __dirname,
-            base: tmp,
-            path: tmp,
-            stat: fs.statSync(tmp)
-        }));
-
-        stream.end();
-    });
-
-    describe('Key-Value pair', function() {
-
-        it('Should remove the access origin if the value is set to false', function(cb) {
-            var tmp = this.tmp;
-            var result = [
-                '<?xml version=\'1.0\' encoding=\'utf-8\'?>',
-                '<widget>',
-                '</widget>'
-            ];
-
-            // Create the stream
-            var stream = access('*', false);
-
-            stream.on('data', function() { });
-
-            stream.on('end', function() {
-                var content = fs.readFileSync(path.join(tmp, 'config.xml'), 'utf8');
-
-                // Assert the content
-                content.should.be.equal(result.join(os.EOL) + os.EOL);
-
-                cb();
-            });
-
-            // Write the file to the stream
-            stream.write(new gutil.File({
-                cwd: __dirname,
-                base: tmp,
-                path: tmp,
-                stat: fs.statSync(tmp)
-            }));
-
-            stream.end();
-        });
-
-        it('Should add the access origin', function(cb) {
-            var tmp = this.tmp;
-            var result = [
-                '<?xml version=\'1.0\' encoding=\'utf-8\'?>',
-                '<widget>',
-                '    <access origin="*" />',
-                '    <access origin="http://www.google.com" />',
-                '</widget>'
-            ];
-
-            // Create the stream
-            var stream = access('http://www.google.com');
-
-            stream.on('data', function() { });
-
-            stream.on('end', function() {
-                var content = fs.readFileSync(path.join(tmp, 'config.xml'), 'utf8');
-
-                // Assert the content
-                content.should.be.equal(result.join(os.EOL) + os.EOL);
-
-                cb();
-            });
-
-            // Write the file to the stream
-            stream.write(new gutil.File({
-                cwd: __dirname,
-                base: tmp,
-                path: tmp,
-                stat: fs.statSync(tmp)
-            }));
-
-            stream.end();
-        });
-
-        it('Should add the access origin with extra options', function(cb) {
-            var tmp = this.tmp;
-            var result = [
-                '<?xml version=\'1.0\' encoding=\'utf-8\'?>',
-                '<widget>',
-                '    <access origin="*" />',
-                '    <access launch-external="yes" origin="tel:*" />',
-                '</widget>'
-            ];
-
-            // Create the stream
-            var stream = access('tel:*', {'launch-external': 'yes'});
-
-            stream.on('data', function() { });
-
-            stream.on('end', function() {
-                var content = fs.readFileSync(path.join(tmp, 'config.xml'), 'utf8');
-
-                // Assert the content
-                content.should.be.equal(result.join(os.EOL) + os.EOL);
-
-                cb();
-            });
-
-            // Write the file to the stream
-            stream.write(new gutil.File({
-                cwd: __dirname,
-                base: tmp,
-                path: tmp,
-                stat: fs.statSync(tmp)
-            }));
-
-            stream.end();
-        });
-    });
-
-    describe('Object', function() {
-
-        it('Should remove the access origin if the value is set to false', function(cb) {
-            var tmp = this.tmp;
-            var result = [
-                '<?xml version=\'1.0\' encoding=\'utf-8\'?>',
-                '<widget>',
-                '</widget>'
-            ];
-
-            // Create the stream
-            var stream = access({
-                '*': false
-            });
-
-            stream.on('data', function() { });
-
-            stream.on('end', function() {
-                var content = fs.readFileSync(path.join(tmp, 'config.xml'), 'utf8');
-
-                // Assert the content
-                content.should.be.equal(result.join(os.EOL) + os.EOL);
-
-                cb();
-            });
-
-            // Write the file to the stream
-            stream.write(new gutil.File({
-                cwd: __dirname,
-                base: tmp,
-                path: tmp,
-                stat: fs.statSync(tmp)
-            }));
-
-            stream.end();
-        });
-
-        it('Should add the access origin', function(cb) {
-            var tmp = this.tmp;
-            var result = [
-                '<?xml version=\'1.0\' encoding=\'utf-8\'?>',
-                '<widget>',
-                '    <access origin="*" />',
-                '    <access origin="http://www.google.com" />',
-                '</widget>'
-            ];
-
-            // Create the stream
-            var stream = access({
-                'http://www.google.com': true
-            });
-
-            stream.on('data', function() { });
-
-            stream.on('end', function() {
-                var content = fs.readFileSync(path.join(tmp, 'config.xml'), 'utf8');
-
-                // Assert the content
-                content.should.be.equal(result.join(os.EOL) + os.EOL);
-
-                cb();
-            });
-
-            // Write the file to the stream
-            stream.write(new gutil.File({
-                cwd: __dirname,
-                base: tmp,
-                path: tmp,
-                stat: fs.statSync(tmp)
-            }));
-
-            stream.end();
-        });
-
-        it('Should add the access origin with extra options', function(cb) {
-            var tmp = this.tmp;
-            var result = [
-                '<?xml version=\'1.0\' encoding=\'utf-8\'?>',
-                '<widget>',
-                '    <access origin="*" />',
-                '    <access launch-external="yes" origin="tel:*" />',
-                '</widget>'
-            ];
-
-            // Create the stream
-            var stream = access({
-                'tel:*': {'launch-external': 'yes'}
-            });
-
-            stream.on('data', function() { });
-
-            stream.on('end', function() {
-                var content = fs.readFileSync(path.join(tmp, 'config.xml'), 'utf8');
-
-                // Assert the content
-                content.should.be.equal(result.join(os.EOL) + os.EOL);
-
-                cb();
-            });
-
-            // Write the file to the stream
-            stream.write(new gutil.File({
-                cwd: __dirname,
-                base: tmp,
-                path: tmp,
-                stat: fs.statSync(tmp)
-            }));
-
-            stream.end();
-        });
-
-        it('Should add and remove the access origin', function(cb) {
-            var tmp = this.tmp;
-            var result = [
-                '<?xml version=\'1.0\' encoding=\'utf-8\'?>',
-                '<widget>',
-                '    <access origin="http://www.google.com" />',
-                '</widget>'
-            ];
-
-            // Create the stream
-            var stream = access({
-                '*': false,
-                'http://www.google.com': true
-            });
-
-            stream.on('data', function() { });
-
-            stream.on('end', function() {
-                var content = fs.readFileSync(path.join(tmp, 'config.xml'), 'utf8');
-
-                // Assert the content
-                content.should.be.equal(result.join(os.EOL) + os.EOL);
-
-                cb();
-            });
-
-            // Write the file to the stream
-            stream.write(new gutil.File({
-                cwd: __dirname,
-                base: tmp,
-                path: tmp,
-                stat: fs.statSync(tmp)
-            }));
-
-            stream.end();
-        });
-
-        it('Should add multiple access origins', function(cb) {
-            var tmp = this.tmp;
-            var result = [
-                '<?xml version=\'1.0\' encoding=\'utf-8\'?>',
-                '<widget>',
-                '    <access origin="*" />',
-                '    <access origin="http://www.google.com" />',
-                '    <access launch-external="yes" origin="tel:*" />',
-                '</widget>'
-            ];
-
-            // Create the stream
-            var stream = access({
-                'http://www.google.com': true,
-                'tel:*': {'launch-external': 'yes'}
-            });
-
-            stream.on('data', function() { });
-
-            stream.on('end', function() {
-                var content = fs.readFileSync(path.join(tmp, 'config.xml'), 'utf8');
-
-                // Assert the content
-                content.should.be.equal(result.join(os.EOL) + os.EOL);
-
-                cb();
-            });
-
-            // Write the file to the stream
-            stream.write(new gutil.File({
-                cwd: __dirname,
-                base: tmp,
-                path: tmp,
-                stat: fs.statSync(tmp)
-            }));
-
-            stream.end();
-        });
-    });
+describe('gulp-cordova-access', function () {
+	beforeEach(function () {
+		this.tmp = tempfile();
+
+		fs.copySync(path.join(__dirname, '/fixtures/config.xml'), path.join(this.tmp, 'config.xml'));
+	});
+
+	it('Should throw an error if something went wrong', function (cb) {
+		var tmp = tempfile();
+
+		fs.copySync(path.join(__dirname, '/fixtures/config.empty.xml'), path.join(tmp, 'config.xml'));
+
+		// Create the stream
+		var stream = access('*', false);
+
+		stream.on('error', function () {
+			cb();
+		});
+
+		// Write the file to the stream
+		stream.write(new gutil.File({
+			cwd: __dirname,
+			base: tmp,
+			path: tmp,
+			stat: fs.statSync(tmp)
+		}));
+
+		stream.end();
+	});
+
+	describe('Key-Value pair', function () {
+		it('Should remove the access origin if the value is set to false', function (cb) {
+			var tmp = this.tmp;
+			var result = [
+				'<?xml version=\'1.0\' encoding=\'utf-8\'?>',
+				'<widget>',
+				'</widget>'
+			];
+
+			// Create the stream
+			var stream = access('*', false);
+
+			stream.on('data', function () { });
+
+			stream.on('end', function () {
+				var content = fs.readFileSync(path.join(tmp, 'config.xml'), 'utf8');
+
+				// Assert the content
+				content.should.be.equal(result.join(os.EOL) + os.EOL);
+
+				cb();
+			});
+
+			// Write the file to the stream
+			stream.write(new gutil.File({
+				cwd: __dirname,
+				base: tmp,
+				path: tmp,
+				stat: fs.statSync(tmp)
+			}));
+
+			stream.end();
+		});
+
+		it('Should add the access origin', function (cb) {
+			var tmp = this.tmp;
+			var result = [
+				'<?xml version=\'1.0\' encoding=\'utf-8\'?>',
+				'<widget>',
+				'    <access origin="*" />',
+				'    <access origin="http://www.google.com" />',
+				'</widget>'
+			];
+
+			// Create the stream
+			var stream = access('http://www.google.com');
+
+			stream.on('data', function () { });
+
+			stream.on('end', function () {
+				var content = fs.readFileSync(path.join(tmp, 'config.xml'), 'utf8');
+
+				// Assert the content
+				content.should.be.equal(result.join(os.EOL) + os.EOL);
+
+				cb();
+			});
+
+			// Write the file to the stream
+			stream.write(new gutil.File({
+				cwd: __dirname,
+				base: tmp,
+				path: tmp,
+				stat: fs.statSync(tmp)
+			}));
+
+			stream.end();
+		});
+
+		it('Should add the access origin with extra options', function (cb) {
+			var tmp = this.tmp;
+			var result = [
+				'<?xml version=\'1.0\' encoding=\'utf-8\'?>',
+				'<widget>',
+				'    <access origin="*" />',
+				'    <access launch-external="yes" origin="tel:*" />',
+				'</widget>'
+			];
+
+			// Create the stream
+			var stream = access('tel:*', {'launch-external': 'yes'});
+
+			stream.on('data', function () { });
+
+			stream.on('end', function () {
+				var content = fs.readFileSync(path.join(tmp, 'config.xml'), 'utf8');
+
+				// Assert the content
+				content.should.be.equal(result.join(os.EOL) + os.EOL);
+
+				cb();
+			});
+
+			// Write the file to the stream
+			stream.write(new gutil.File({
+				cwd: __dirname,
+				base: tmp,
+				path: tmp,
+				stat: fs.statSync(tmp)
+			}));
+
+			stream.end();
+		});
+	});
+
+	describe('Object', function () {
+		it('Should remove the access origin if the value is set to false', function (cb) {
+			var tmp = this.tmp;
+			var result = [
+				'<?xml version=\'1.0\' encoding=\'utf-8\'?>',
+				'<widget>',
+				'</widget>'
+			];
+
+			// Create the stream
+			var stream = access({
+				'*': false
+			});
+
+			stream.on('data', function () { });
+
+			stream.on('end', function () {
+				var content = fs.readFileSync(path.join(tmp, 'config.xml'), 'utf8');
+
+				// Assert the content
+				content.should.be.equal(result.join(os.EOL) + os.EOL);
+
+				cb();
+			});
+
+			// Write the file to the stream
+			stream.write(new gutil.File({
+				cwd: __dirname,
+				base: tmp,
+				path: tmp,
+				stat: fs.statSync(tmp)
+			}));
+
+			stream.end();
+		});
+
+		it('Should add the access origin', function (cb) {
+			var tmp = this.tmp;
+			var result = [
+				'<?xml version=\'1.0\' encoding=\'utf-8\'?>',
+				'<widget>',
+				'    <access origin="*" />',
+				'    <access origin="http://www.google.com" />',
+				'</widget>'
+			];
+
+			// Create the stream
+			var stream = access({
+				'http://www.google.com': true
+			});
+
+			stream.on('data', function () { });
+
+			stream.on('end', function () {
+				var content = fs.readFileSync(path.join(tmp, 'config.xml'), 'utf8');
+
+				// Assert the content
+				content.should.be.equal(result.join(os.EOL) + os.EOL);
+
+				cb();
+			});
+
+			// Write the file to the stream
+			stream.write(new gutil.File({
+				cwd: __dirname,
+				base: tmp,
+				path: tmp,
+				stat: fs.statSync(tmp)
+			}));
+
+			stream.end();
+		});
+
+		it('Should add the access origin with extra options', function (cb) {
+			var tmp = this.tmp;
+			var result = [
+				'<?xml version=\'1.0\' encoding=\'utf-8\'?>',
+				'<widget>',
+				'    <access origin="*" />',
+				'    <access launch-external="yes" origin="tel:*" />',
+				'</widget>'
+			];
+
+			// Create the stream
+			var stream = access({
+				'tel:*': {'launch-external': 'yes'}
+			});
+
+			stream.on('data', function () { });
+
+			stream.on('end', function () {
+				var content = fs.readFileSync(path.join(tmp, 'config.xml'), 'utf8');
+
+				// Assert the content
+				content.should.be.equal(result.join(os.EOL) + os.EOL);
+
+				cb();
+			});
+
+			// Write the file to the stream
+			stream.write(new gutil.File({
+				cwd: __dirname,
+				base: tmp,
+				path: tmp,
+				stat: fs.statSync(tmp)
+			}));
+
+			stream.end();
+		});
+
+		it('Should add and remove the access origin', function (cb) {
+			var tmp = this.tmp;
+			var result = [
+				'<?xml version=\'1.0\' encoding=\'utf-8\'?>',
+				'<widget>',
+				'    <access origin="http://www.google.com" />',
+				'</widget>'
+			];
+
+			// Create the stream
+			var stream = access({
+				'*': false,
+				'http://www.google.com': true
+			});
+
+			stream.on('data', function () { });
+
+			stream.on('end', function () {
+				var content = fs.readFileSync(path.join(tmp, 'config.xml'), 'utf8');
+
+				// Assert the content
+				content.should.be.equal(result.join(os.EOL) + os.EOL);
+
+				cb();
+			});
+
+			// Write the file to the stream
+			stream.write(new gutil.File({
+				cwd: __dirname,
+				base: tmp,
+				path: tmp,
+				stat: fs.statSync(tmp)
+			}));
+
+			stream.end();
+		});
+
+		it('Should add multiple access origins', function (cb) {
+			var tmp = this.tmp;
+			var result = [
+				'<?xml version=\'1.0\' encoding=\'utf-8\'?>',
+				'<widget>',
+				'    <access origin="*" />',
+				'    <access origin="http://www.google.com" />',
+				'    <access launch-external="yes" origin="tel:*" />',
+				'</widget>'
+			];
+
+			// Create the stream
+			var stream = access({
+				'http://www.google.com': true,
+				'tel:*': {'launch-external': 'yes'}
+			});
+
+			stream.on('data', function () { });
+
+			stream.on('end', function () {
+				var content = fs.readFileSync(path.join(tmp, 'config.xml'), 'utf8');
+
+				// Assert the content
+				content.should.be.equal(result.join(os.EOL) + os.EOL);
+
+				cb();
+			});
+
+			// Write the file to the stream
+			stream.write(new gutil.File({
+				cwd: __dirname,
+				base: tmp,
+				path: tmp,
+				stat: fs.statSync(tmp)
+			}));
+
+			stream.end();
+		});
+	});
 });
